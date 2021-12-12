09fe2f8f0ac1fc1c7589331db87bf7d4fe0ef70b
Nanci <nanci.uicabbalam@itsva.edu.mx>
2021-12-12 17:47:58 -0500

    cambios al nav

--- vistas/paginas/productos.php
+++ vistas/paginas/productos.php
@@ -42,7 +42,7 @@ if($_SESSION["s_usuario"] === null){
         <!-- ============================================================== -->
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                <a class="navbar-brand" href="../pag_inicio">SISTEMA DE INVENTARIOS</a>
+                <a class="navbar-brand" href="../pag_inicio">SOFTRECORD</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" 
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
                     aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
